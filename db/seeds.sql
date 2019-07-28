f0bc1c18f7e5a3256ff5855726d66e47619d7518
estevan <estevan.renteria@gmail.com>
2019-07-27 23:25:37 -0700

    eat-da-burger

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Western Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali-Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Quarter Pounder Burger', true);
